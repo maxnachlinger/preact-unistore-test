93e09a5c209fd3a1d304c890e87f9d110c2c4b5c
Max Nachlinger <mnachlinger@walmartlabs.com>
2018-08-29 16:39:55 -0700

    Code clean-up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,5 @@
 import { search } from './search';
 
-// If actions is a function, it gets passed the store:
 export const actions = (store) => ({
   search: (...args) => search(store, ...args),
-
-  // Actions can just return a state update:
-  increment: ({ count }) => ({ count: count + 1 }),
-
-  // Actions receive current state as first parameter and any other params next
-  incrementAndLog: ({ count }) => {
-    console.info('incrementAndLog');
-    return { count: count + 1 };
-  },
-
-  // Async actions can be pure async/promise functions:
-  // async getStuff(state) {
-  //   let res = await fetch('/foo.json');
-  //   return { stuff: await res.json() };
-  // },
-
-  // ... or just actions that call store.setState() later:
-  incrementAsync(state) {
-    setTimeout(() => {
-      console.info('incrementAsync');
-      store.setState({ count: state.count + 1 });
-    }, 100);
-  },
 });
