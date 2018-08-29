b403b7682d0c968fd68752fac6ef122b6aa3911e
Max Nachlinger <max.nachlinger@gmail.com>
2018-08-29 15:52:26 -0700

    Dropped async await for now

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 ## preact-unistore-test
 
 ### Getting Started
+
 Install dependencies:
+
 ```
 npm i
 ```
+
 Start:
+
 ```
 npm start
 ```
+
 Go to: [http://127.0.0.1:2990](http://127.0.0.1:2990)
--- src/actions/search.js
+++ src/actions/search.js
@@ -1,18 +1,15 @@
 import fetch from 'unfetch';
 import { SearchStatus } from '../consts';
 
-export const search = async (store, state, term) => {
+export const search = (store, state, term) => {
   store.setState({ ...state, results: [], searchStatus: SearchStatus.Processing });
 
-  const response = await fetch(
-    `http://search.mobile.walmart.com/v1/browse/search?query=${term}`,
-  ).catch((error) => {
-    store.setState({ ...state, results: [], searchStatus: SearchStatus.Error });
-    console.error(error);
-    throw error;
-  });
-
-  const { item: results } = await response.json();
-
-  return { ...state, results, searchStatus: SearchStatus.Ok };
+  return fetch(`http://search.mobile.walmart.com/v1/browse/search?query=${term}`)
+    .then((response) => response.json())
+    .then(({ item: results }) => ({ ...state, results, searchStatus: SearchStatus.Ok }))
+    .catch((error) => {
+      store.setState({ ...state, results: [], searchStatus: SearchStatus.Error });
+      console.error(error);
+      throw error;
+    });
 };
