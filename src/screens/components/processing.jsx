8b9f2d1ec7e33a38d109d3deb72e185fba39d7cb
Max Nachlinger <max.nachlinger@gmail.com>
2018-08-29 17:38:47 -0700

    Made the screens easier on the eyes

--- src/screens/components/processing.jsx
+++ src/screens/components/processing.jsx
@@ -1,3 +1,7 @@
 import { h, render } from 'preact';
 
-export const ProcessingScreen = () => <div className="processing-screen">Processing</div>;
+export const ProcessingScreen = () => (
+  <div class="processing-screen">
+    <div class="loader">Processing...</div>
+  </div>
+);
